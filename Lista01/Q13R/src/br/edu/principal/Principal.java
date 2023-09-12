e948749b9ee21df3a3387120675e74f3933ae714
elbs.sz <124272676+Elbston@users.noreply.github.com>
2023-09-12 13:54:29 -0300

    Update Principal.java

--- Lista01/Q13R/src/br/edu/principal/Principal.java
+++ Lista01/Q13R/src/br/edu/principal/Principal.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		double medpol = 12, medjrd = 3, medmh = 1.760;
-		
-
+		Double pes, pol, medjrd, medmh;
+		Scanner prompt = new Scanner(System.in);
+		pes = prompt.nextDouble();
+		pol = 12*pes;
+		medjrd = 3*pes;
+		medmh = medjrd/ 1.760;
+		System.out.println("As medidas são: polegada: " + pol + "Jardas: " + medjrd + "Milha: " + medmh);  
 	}
 
 }
