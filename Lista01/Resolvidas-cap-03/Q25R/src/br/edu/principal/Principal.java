5966b49df26ca8a51708d0c8642ac901268258e3
elbs.sz <124272676+Elbston@users.noreply.github.com>
2023-10-05 11:26:32 -0300

    Update Principal.java

--- Lista01/Resolvidas-cap-03/Q25R/src/br/edu/principal/Principal.java
+++ Lista01/Resolvidas-cap-03/Q25R/src/br/edu/principal/Principal.java
@@ -1,9 +1,20 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner tlcd = new Scanner(System.in);
+        double custo, convite, qtd;
+
+        System.out.print("Informe o custo total: ");
+        custo = tlcd.nextDouble();
+
+        System.out.print("Informe o preço por convite: ");
+        convite = tlcd.nextDouble();
+
+        qtd = custo / convite;
 
 	}
 
